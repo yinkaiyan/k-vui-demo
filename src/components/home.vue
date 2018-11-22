a575dfc9adf67d263bb3f27fb485cfd2132bf969
yinkaiyan <1617768751@qq.com>
2018-11-22 21:06:02 +0800

    导航栏

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,12 @@
-<style scoped>
-  
+<style scoped lang="less">
+  @import "./home/home.less";
 </style>
 <template>
   <div class="home_xm">
     <router-view></router-view>
     <div class="home_xm_bottom">
       <i class="icon iconfont">&#xe600;</i>
+      <div class="home_xm_list"></div>
     </div>
   </div>
 </template>
