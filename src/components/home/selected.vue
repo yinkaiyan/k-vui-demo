6a7e1b5c368762796b83c4c0893c127ed53a9b74
yinkaiyan <1617768751@qq.com>
2018-11-23 23:16:16 +0800

    搜索

--- src/components/home/selected.vue
+++ src/components/home/selected.vue
@@ -1,30 +1,51 @@
+<style scoped lang="less">
+  @import "./home.less";
+</style>
 <template>
   <div>
-    <div>
-      精选
+    <div class="selected-home">
+      <div class="selected-title">
+        <div class="selected-tab">
+          <div class="selected-tab-list tabActive">推荐</div>
+          <div class="selected-tab-list">电台</div>
+        </div>
+        <div class="selected-search">
+          <search
+            v-model="value"
+            @on-focus="onFocus"
+            ></search>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import { Search, Group, Cell, XButton } from 'vux'
 export default {
+  components: {
+    Search,
+    Group,
+    Cell,
+    XButton
+  },
+
   data () {
     return {
       show1: true,
+      results: [],
+      value: ''
     }
   },
   methods: {
-    log (str) {
-      console.log(str)
-    }
+ 
+   
+    onFocus () {
+      console.log('on focus')
+    },
   },
   watch: {
    
   }
 }
 </script>
-
-<style lang="less" scoped>
-
-</style>
